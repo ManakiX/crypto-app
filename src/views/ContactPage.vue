8d1285751b24209f710db43aaaf377200ca29835
unknown <sogeyanaluntu@gmail.com>
2023-08-29 16:16:36 +0200

    overhaul

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -1,74 +1,329 @@
-<template>
-    <div class="container">
-      <div class="contact-content">
-        <h2>Contact Us</h2>
-        <p class="contact-details">Sharenet Address: 301, 3rd Floor Imperial Terraces, Tyger Waterfront, Carl Cronje Drive, Bellville, Cape Town, 7530</p>
-        <p class="contact-details">Contact Numbers: (021) 700 4800, +27 21 700 4800 (International)</p>
-        <h3>Location</h3>
-        <div class="map-container">
-          <div class="map-frame">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13249.979818258218!2d18.6301076!3d-33.8769045!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc41e6218f7ab3%3A0xbdd802ca53b92d43!2sSharenet%20(Pty)%20Ltd!5e0!3m2!1sen!2sza!4v1692789010035!5m2!1sen!2sza"
-              width="600"
-              height="450"
-              style="border: 0;"
-              loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
-            ></iframe>
-          </div>
+<!-- <template>
+  <div class="container">
+    <div class="contact-content">
+      <div class="map-container">
+        <div class="map-frame">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13249.979818258218!2d18.6301076!3d-33.8769045!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc41e6218f7ab3%3A0xbdd802ca53b92d43!2sSharenet%20(Pty)%20Ltd!5e0!3m2!1sen!2sza!4v1692789010035!5m2!1sen!2sza"
+            width="600"
+            height="450"
+            style="border: 0;"
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
+      </div>
+      <div class="contact-form-container">
+        <h2>Contact Us</h2>
+        <form class="contact-form">
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="name" required>
+          
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email" required>
+          
+          <label for="message">Message:</label>
+          <textarea id="message" name="message" rows="4" required></textarea>
+          
+          <button type="submit">Submit</button>
+        </form>
         <router-link class="back-link" to="/">Back to Home</router-link>
       </div>
     </div>
-  </template>
-  
-  <style>
-  .container {
-    max-width: 800px;
-    margin: 0 auto;
-    padding: 20px;
-  }
-  
-  .contact-content {
-    background-color: #f7f7f7;
-    padding: 20px;
-    border-radius: 5px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  }
-  
-  .contact-details {
-    font-size: 18px;
-    margin: 10px 0;
-  }
-  
-  .map-container {
-    margin-top: 20px;
-  }
-  
-  .map-frame {
-    position: relative;
-    padding-bottom: 60%; /* Aspect ratio for the map frame */
-    height: 0;
-    overflow: hidden;
-  }
-  
-  /* Styling for the "Back to Home" link */
-  .back-link {
-    display: block;
-    margin-top: 20px;
-    font-weight: bold;
-    color: #333;
-    text-decoration: none;
-  }
-  
-  .back-link:hover {
-    text-decoration: underline;
-  }
-  </style>
-  
-  
-  
-  
-  
-  
-  
+  </div>
+</template>
+
+<style>
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.contact-content {
+  display: flex;
+  background-color: #f7f7f7;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.map-container {
+  width: 50%;
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+.map-frame {
+  position: relative;
+  padding-bottom: 140%; /* Aspect ratio for the map frame */
+  height: 0;
+  overflow: hidden;
+}
+
+.contact-form-container {
+  width: 50%;
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+.contact-form {
+  display: flex;
+  flex-direction: column;
+}
+
+.contact-form label {
+  margin-top: 10px;
+  font-weight: bold;
+}
+
+.contact-form input,
+.contact-form textarea {
+  padding: 8px;
+  margin-top: 5px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+}
+
+.contact-form button {
+  margin-top: 15px;
+  padding: 8px 15px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+}
+
+.contact-form button:hover {
+  background-color: #0056b3;
+}
+
+/* Styling for the "Back to Home" link */
+.back-link {
+  display: block;
+  margin-top: 20px;
+  font-weight: bold;
+  color: #333;
+  text-decoration: none;
+}
+
+.back-link:hover {
+  text-decoration: underline;
+}
+</style> -->
+
+<template>
+  <body>
+    <div class="container">
+      <div class="contact-content">
+        <div class="contact-info">
+          <h2>TALK TO US ON WhatsApp</h2>
+          <p>Chat with us on WhatsApp by scanning the QR code to add Sharenet as a contact.</p>
+          <h3>How it works</h3>
+          <p>Open WhatsApp to chat with us on your computer, or scan the QR code to add Sharenet as a contact.</p>
+          <p>Say "Hi" to us in the WhatsApp chat to start the conversation.</p>
+          <p>Follow the prompts to complete your request.</p>
+        </div>
+        <div class="contact-form-container">
+          <h2>COMPLETE THE FORM BELOW AND WE WILL GET BACK TO YOU</h2>
+          <form class="contact-form">
+            <label for="title">Title</label>
+            <select id="title" name="title" required>
+              <option value="" disabled selected>Select an option</option>
+              <option value="Mr">Mr</option>
+              <option value="Mrs">Mrs</option>
+              <option value="Miss">Miss</option>
+              <option value="Dr">Dr</option>
+            </select>
+            
+            <label for="firstNames">First Names</label>
+            <input type="text" id="firstNames" name="firstNames" required>
+            
+            <label for="surname">Surname</label>
+            <input type="text" id="surname" name="surname" required>
+            
+            <label for="email">Email Address</label>
+            <input type="email" id="email" name="email" required>
+            
+            <label for="phone">Phone Number</label>
+            <div class="phone-input">
+              <select id="phoneCode" name="phoneCode" required>
+                <option value="+27">+27</option>
+                <option value="+04">+04</option>
+                <option value="+1">+1</option>
+                <option value="+506">+506</option>
+              </select>
+              <input type="tel" id="phone" name="phone" required>
+            </div>
+            
+            <label for="message">Message</label>
+            <textarea id="message" name="message" rows="4" maxlength="1000" required></textarea>
+            
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+      
+      <div class="map-section">
+        <div class="contact-details">
+          <h2>FIND US HERE</h2>
+          <h3>HEAD OFFICE</h3>
+          <p>301, 3rd Floor Imperial Terraces<br>Tyger Waterfront<br>Carl Cronje Drive<br>Bellville, Cape Town<br>7530</p>
+          <h3>TELEPHONE</h3>
+          <p>(021) 700 4800<br>+27 21 700 4800 (International)</p>
+          <h3>EMAIL</h3>
+          <p>support@sharenet.co.za</p>
+        </div>
+        <div class="map-container">
+          <div class="map-frame">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13249.979818258218!2d18.6301076!3d-33.8769045!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc41e6218f7ab3%3A0xbdd802ca53b92d43!2sSharenet%20(Pty)%20Ltd!5e0!3m2!1sen!2sza!4v1692789010035!5m2!1sen!2sza" width="100%" height="100%" style="border: 0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</template>
+
+<style>
+body {
+  background: rgb(63,94,251);
+background: radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%);
+}
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.contact-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  gap: 20px;
+}
+
+.contact-info,
+.contact-form-container {
+  flex: 1;
+}
+
+.contact-form {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.contact-form label {
+  font-weight: bold;
+}
+
+.contact-form input,
+.contact-form textarea {
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+}
+
+.contact-form button {
+  margin-top: 15px;
+  padding: 8px 15px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+}
+
+.map-section {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin-top: 20px;
+  background: white;
+  border-radius: 10px;
+
+}
+
+.map-container {
+  flex: 1;
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+.map-frame {
+  position: relative;
+  height: 0;
+  padding-bottom: 100%; /* 16:9 aspect ratio */
+  padding-right: 100%;
+  overflow: hidden;
+}
+
+.map-frame iframe {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  border: 0;
+}
+
+.contact-details {
+  flex: 1;
+  padding: 20px;
+  border-top: 1px solid #ccc;
+}
+
+.details {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.detail-item h3 {
+  margin-bottom: 10px;
+}
+
+.back-link {
+  display: block;
+  margin-top: 20px;
+  font-weight: bold; 
+  color: #333;
+  text-decoration: none;
+}
+
+.back-link:hover {
+  text-decoration: underline;
+}
+
+#phone {
+  width: 1000px;
+  padding-left: 54%;
+}
+
+.phone-input {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  max-width: 250px; /* Adjust the width as needed */
+}
+
+.phone-input select,
+.phone-input input {
+  flex: 1;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+}
+
+/* Additional styling adjustments */
+.contact-details {
+  flex: 1;
+  padding: 20px;
+  border-top: 1px solid #ccc;
+  border-radius: 10px; /* Rounded border for the contact details */
+}
+
+</style>
+
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,104 +1,83 @@
 <template>
-    <div class="container">
-      <table class="crypto-table">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th @click="sort('name')">Coin Name</th>
-            <th @click="sort('symbol')">Coin Symbol</th>
-            <th @click="sort('current_price')">Price</th>
-            <th @click="sort('price_change_percentage_1h_in_currency')">1h Move</th>
-            <th @click="sort('market_cap')">Market Cap</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="crypto in sortedCrypto" :key="crypto.id">
-            <td class="rank">{{ crypto.rank }}</td>
-            <td>
-              <img :src="crypto.image" :alt="crypto.name" class="coin-icon" />
-              {{ crypto.name }}
-            </td>
-            <td>{{ crypto.symbol }}</td>
-            <td>{{ 'ZAR ' + crypto.current_price.toFixed(2) }}</td>
-            <td>{{ crypto.price_change_percentage_1h_in_currency }}</td>
-            <td>{{ 'ZAR ' + crypto.market_cap.toLocaleString() }}</td>
-          </tr>
-        </tbody>
-      </table>
-      <router-link class="contact-link" to="/contact">Contact Us</router-link>
+  <div class="container">
+    <div class="loading-overlay" v-if="isLoading">
+      <div class="loading-spinner"></div>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        cryptoData: [],
-        sortKey: '',
-        sortOrder: 1,
-      };
+    <table class="spot-table" v-else>
+      <thead class="tbl-header">
+        <tr>
+          <th>#</th>
+          <th @click="sort('FullName')">Coin Name</th>
+          <th @click="sort('Price')">Price</th>
+          <th @click="sort('Move')">Move</th>
+          <th @click="sort('PercentageMove')">Percentage Move</th>
+          <th @click="sort('Time')">Time</th>
+        </tr>
+      </thead>
+      <tbody class="tbl-content">
+        <tr v-for="spot in sortedSpotData" :key="spot.Id">
+          <td class="rank">{{ spot.Id }}</td>
+          <td>{{ spot.FullName }}</td>
+          <td>{{ 'ZAR ' + spot.Price.toFixed(2) }}</td>
+          <td>{{ spot.Move }}</td>
+          <td>{{ spot.PercentageMove.toFixed(2) }}%</td>
+          <td>{{ spot.Time }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <router-link class="contact-link" to="/contact">Contact Us</router-link>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      isLoading: true,
+      spotData: [],
+      sortKey: '',
+      sortOrder: 1,
+    };
+  },
+  computed: {
+    sortedSpotData() {
+      const sorted = this.spotData.slice().sort((a, b) => {
+        const keyA = a[this.sortKey];
+        const keyB = b[this.sortKey];
+        return this.sortOrder * (keyA > keyB ? 1 : -1);
+      });
+      return sorted;
     },
-    computed: {
-      sortedCrypto() {
-        const sorted = this.cryptoData.slice().sort((a, b) => {
-          const keyA = a[this.sortKey];
-          const keyB = b[this.sortKey];
-          return this.sortOrder * (keyA > keyB ? 1 : -1);
-        });
-        return sorted;
-      },
+  },
+  methods: {
+    async fetchSpotData() {
+      try {
+        const response = await axios.get('https://api.sharenet.co.za/api/v1/px2/spots');
+        this.spotData = response.data;
+        this.isLoading = false;
+      } catch (error) {
+        console.error('Error fetching spot data:', error);
+      }
     },
-    methods: {
-      async fetchData() {
-        try {
-          const response = await axios.get(
-            'https://api.coingecko.com/api/v3/coins/markets',
-            {
-              params: {
-                vs_currency: 'zar',
-                ids: 'bitcoin,ethereum,ripple,cardano,polkadot,binancecoin,dogecoin,litecoin,uniswap,avalanche-2',
-                order: 'market_cap_desc',
-                per_page: 10,
-                page: 1,
-                sparkline: false,
-                localization: false,
-              },
-            }
-          );
-          this.cryptoData = response.data;
-          const coinIds = this.cryptoData.map((crypto) => crypto.id).join(',');
-          const metadataResponse = await axios.get(
-            `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${coinIds}`
-          );
-          const metadataMap = new Map(metadataResponse.data.map((crypto) => [crypto.id, crypto]));
-          this.cryptoData.forEach((crypto, index) => {
-            if (metadataMap.has(crypto.id)) {
-              crypto.image = metadataMap.get(crypto.id).image;
-              crypto.rank = index + 1;
-            }
-          });
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      },
-      sort(key) {
-        if (this.sortKey === key) {
-          this.sortOrder *= -1;
-        } else {
-          this.sortKey = key;
-          this.sortOrder = 1;
-        }
-      },
+    sort(key) {
+      if (this.sortKey === key) {
+        this.sortOrder *= -1;
+      } else {
+        this.sortKey = key;
+        this.sortOrder = 1;
+      }
     },
-    mounted() {
-      this.fetchData();
-    },
-  };
-  </script>
+  },
+  mounted() {
+    this.fetchSpotData();
+  },
+};
+</script>
   
   <style>
+  
   .container {
     max-width: 800px;
     margin: 0 auto;
@@ -108,7 +87,7 @@
   .crypto-table {
     width: 100%;
     border-collapse: collapse;
-    border: 1px solid #ccc;
+    border: 1px solid grey;
     margin-top: 20px;
   }
   
@@ -116,11 +95,11 @@
   td {
     padding: 10px;
     text-align: left;
-    border-bottom: 1px solid #ccc;
+    border-bottom: 1px solid grey;
   }
   
   th {
-    background-color: #f2f2f2;
+    background-color: white;
     cursor: pointer;
   }
   
@@ -149,6 +128,93 @@
   .contact-link:hover {
     text-decoration: underline;
   }
+
+  .loading-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 0.8);
+  z-index: 1;
+}
+
+.loading-spinner {
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  border-top: 4px solid goldenrod;
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  animation: spin 1s linear infinite;
+}
+
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.crypto-table {
+  width: 100%;
+  table-layout: fixed;
+  border-collapse: collapse;
+  border: 1px solid rgba(255, 255, 255, 0.3);
+  margin-top: 20px;
+}
+
+.tbl-header {
+  background-color: rgba(255, 255, 255, 0.3);
+}
+
+.tbl-content {
+  height: 300px;
+  overflow-x: auto;
+  margin-top: 0px;
+  border: 1px solid rgba(255, 255, 255, 0.3);
+}
+
+th {
+  padding: 20px 15px;
+  text-align: left;
+  font-weight: 500;
+  font-size: 12px;
+  color: black;
+  text-transform: uppercase;
+}
+
+td {
+  padding: 15px;
+  text-align: left;
+  vertical-align: middle;
+  font-weight: 300;
+  font-size: 12px;
+  color: black;
+  border-bottom: solid 1px rgba(255, 255, 255, 0.1);
+}
+
+.contact-link {
+  display: block;
+  margin-top: 20px;
+  font-weight: bold;
+  color: #333;
+  text-decoration: none;
+}
+
+.contact-link:hover {
+  text-decoration: underline;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
   </style>
   
 
